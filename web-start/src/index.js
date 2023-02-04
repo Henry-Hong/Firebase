bae3704c755f487d846fa779e0a137e0cfeb6081
Henry-Hong <hhr8540@naver.com>
2023-02-04 18:09:31 +0900

    [web-start] sing-out, sign-in subscriber, getProfileURL, userName..

--- web-start/src/index.js
+++ web-start/src/index.js
@@ -52,32 +52,37 @@ async function signIn() {
 	// TODO 1: Sign in Firebase with credential from the Google user.
 	const provider = new GoogleAuthProvider();
 	await signInWithPopup(getAuth(), provider);
-	// QUESTION: getAuth() 의 역할?
+	// QUESTION: getAuth() 의 역할? -> auth가 (모든?)사용자 정보를 담고 있나보다.
 }
 
 // Signs-out of Friendly Chat.
 function signOutUser() {
 	// TODO 2: Sign out of Firebase.
+	signOut(getAuth());
 }
 
 // Initiate firebase auth
 function initFirebaseAuth() {
 	// TODO 3: Subscribe to the user's signed-in status
+	onAuthStateChanged(getAuth(), authStateObserver);
 }
 
 // Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
 	// TODO 4: Return the user's profile pic URL.
+	return getAuth().currentUser.photoURL || "/images/profile_placeholder.png";
 }
 
 // Returns the signed-in user's display name.
 function getUserName() {
 	// TODO 5: Return the user's display name.
+	return getAuth().currentUser.displayName || "default";
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
 	// TODO 6: Return true if a user is signed-in.
+	return !!getAuth().currentUser; // currentUser return null when not logged - in
 }
 
 // Saves a new message on the Cloud Firestore.
